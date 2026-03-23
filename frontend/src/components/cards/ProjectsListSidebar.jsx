2dd8b57f8f4bda4be659feaaf966cc703ab3d1b9
yassir laa <yassirlaa411@gmail.com>
2026-03-23 14:13:54 +0100

    testing

--- frontend/src/components/cards/ProjectsListSidebar.jsx
+++ frontend/src/components/cards/ProjectsListSidebar.jsx
@@ -11,7 +11,32 @@ function ProjectsListSidebar({ projects, cur_project }) {
     cancelled: styles.cancelled,
   };
 
-  return <div>hello</div>
+  return (
+    <div className={styles.projectsTitlesSection}>
+      <h3 className={styles.projectsHeader}>Projects</h3>
+      <div className={styles.projectsTitlesContainer}>
+        {projects.map((project) => (
+          <NavLink
+            to={`/dashboard/client/proposals?project_id=${project.id}`}
+            key={project.id}
+            className={(_) =>
+              +cur_project === project.id
+                ? `${styles.projectTitleCard} ${styles.selected}`
+                : `${styles.projectTitleCard}`
+            }
+          >
+            <div className={styles.cardLeft}>
+              <h4>{project.title}</h4>
+              <p>Number of proposals : {project.proposalsCount}</p>
+            </div>
+            <div className={`${styles.cardRight} ${classes[project.status]}`}>
+              <span className={styles.projectStatus}>{project.status}</span>
+            </div>
+          </NavLink>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsListSidebar;
