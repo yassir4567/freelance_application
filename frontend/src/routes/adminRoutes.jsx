563989ff1a5e5bf3e99ee399135ab07c797f68ed
mohamedbelkassmi3 <mohamedbelkassmi3@gmail.com>
2026-04-12 19:29:02 +0100

    admin

--- frontend/src/routes/adminRoutes.jsx
+++ frontend/src/routes/adminRoutes.jsx
@@ -4,6 +4,8 @@ import AdminDashboard from "../pages/admin/AdminDashboardPage";
 import AdminProjectsPage from "../pages/admin/AdminProjectsPage";
 import AdminSkillsPage from "../pages/admin/AdminSkillsPage";
 import AdminUsersPage from "../pages/admin/AdminUsersPage";
+import UserDetails from "../pages/admin/UserDetails";
+
 
 export const adminRoutes = [
   {
@@ -12,6 +14,7 @@ export const adminRoutes = [
     children: [
       { index: true, element: <AdminDashboard /> },
       { path: "users", element: <AdminUsersPage /> },
+      { path: "userdetails", element: <UserDetails /> },
       { path: "projects", element: <AdminProjectsPage /> },
       { path: "categories", element: <AdminCategoriePage /> },
       { path: "skills", element: <AdminSkillsPage /> },
