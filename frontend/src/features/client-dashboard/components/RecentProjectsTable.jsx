4e6f27c5bb1e6bc4a25b75d42117d37ab6022f67
yassir laa <yassirlaa411@gmail.com>
2026-04-21 21:13:34 +0100

    fix: fix empty table message in dashboard

--- frontend/src/features/client-dashboard/components/RecentProjectsTable.jsx
+++ frontend/src/features/client-dashboard/components/RecentProjectsTable.jsx
@@ -8,6 +8,8 @@ function RecentProjectsTable({ projects }) {
     completed: "status-purple",
     cancelled: "status-danger",
   };
+
+  const hasProjects = projects?.length;
   return (
     <table className={styles.table}>
       <thead>
@@ -20,28 +22,36 @@ function RecentProjectsTable({ projects }) {
         </tr>
       </thead>
       <tbody>
-        {projects?.map((project) => (
-          <tr key={project.id}>
-            <td className={styles.titleCell}>
-              <span>{project.title}</span>
-            </td>
-            <td>
-              <span
-                className={`${styles.status} ${statusClasses[project.status]}`}
-              >
-                {project.status.split("_").join(" ")}
-              </span>
-            </td>
-            <td className={styles.numberProposalTd}>
-              <span>{project.proposals_count}</span>
-            </td>
-            <td>{getRelativeTime(project.created_at)}</td>
-            <td className={styles.actions}>
-              <button className={styles.actionBtn}>View Details</button>
-              <button className={styles.actionBtn}>View Proposals</button>
+        {hasProjects ? (
+          projects?.map((project) => (
+            <tr key={project.id}>
+              <td className={styles.titleCell}>
+                <span>{project.title}</span>
+              </td>
+              <td>
+                <span
+                  className={`${styles.status} ${statusClasses[project.status]}`}
+                >
+                  {project.status.split("_").join(" ")}
+                </span>
+              </td>
+              <td className={styles.numberProposalTd}>
+                <span>{project.proposals_count}</span>
+              </td>
+              <td>{getRelativeTime(project.created_at)}</td>
+              <td className={styles.actions}>
+                <button className={styles.actionBtn}>View Details</button>
+                <button className={styles.actionBtn}>View Proposals</button>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colspan="5" className={styles.empty}>
+              You don't have active contracts yet
             </td>
           </tr>
-        ))}
+        )}
       </tbody>
     </table>
   );
--- frontend/src/features/freelancer-dashboard/components/FreelancerActiveContracts.jsx
+++ frontend/src/features/freelancer-dashboard/components/FreelancerActiveContracts.jsx
@@ -4,6 +4,8 @@ import { formatDate } from "../../../utils/helpers";
 import avatar from "../../../assets/images/profile.png";
 
 function FreelancerActiveContracts({ contracts }) {
+  const hasContracts = contracts?.length > 0;
+
   return (
     <table className={styles.table}>
       <thead>
@@ -16,38 +18,49 @@ function FreelancerActiveContracts({ contracts }) {
         </tr>
       </thead>
       <tbody>
-        {contracts?.map((contract) => {
-          const {
-            id,
-            final_price,
-            final_deadline,
-            proposal: {
-              project: {
-                title,
-                client: { first_name, last_name },
+        {hasContracts ? (
+          contracts?.map((contract) => {
+            const {
+              id,
+              final_price,
+              final_deadline,
+              proposal: {
+                project: {
+                  title,
+                  client: { first_name, last_name },
+                },
               },
-            },
-          } = contract;
+            } = contract;
 
-          return (
-            <tr key={id} className={styles.row}>
-              <td>{title}</td>
-              <td>
-                <div className={styles.clientTD}>
-                  <img src={avatar} width={37} alt="Avatar" />
-                  <span>
-                    {first_name} {last_name}
-                  </span>
-                </div>
-              </td>
-              <td className={styles.budget}> ${final_price} </td>
-              <td className={styles.deadline}> {formatDate(final_deadline)}</td>
-              <td>
-                <NavLink className={styles.link}>View details</NavLink>
-              </td>
-            </tr>
-          );
-        })}
+            return (
+              <tr key={id} className={styles.row}>
+                <td>{title}</td>
+                <td>
+                  <div className={styles.clientTD}>
+                    <img src={avatar} width={37} alt="Avatar" />
+                    <span>
+                      {first_name} {last_name}
+                    </span>
+                  </div>
+                </td>
+                <td className={styles.budget}> ${final_price} </td>
+                <td className={styles.deadline}>
+                  {" "}
+                  {formatDate(final_deadline)}
+                </td>
+                <td>
+                  <NavLink className={styles.link}>View details</NavLink>
+                </td>
+              </tr>
+            );
+          })
+        ) : (
+          <tr>
+            <td colspan="5" className={styles.empty}>
+              You don't have active contracts yet
+            </td>
+          </tr>
+        )}
       </tbody>
     </table>
   );
