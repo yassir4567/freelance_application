8e5469c68e3e30c45ff88fd900ca63cc6f03f0df
yassir laa <yassirlaa411@gmail.com>
2026-04-28 22:15:39 +0100

    create get client contracts and stats request functions

---
+++ frontend/src/api/contracts/getClientContracts.js
@@ -0,0 +1,37 @@
+import { BASE_URL, getToken } from "../config";
+
+const getClientContracts = async () => {
+  try {
+    const token = getToken();
+    const response = await fetch(`${BASE_URL}/client/contracts`, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        ...(token && { Authorization: `Bearer ${token}` }),
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return {
+        success: false,
+        message: data.message || "Error in fetching client contracts",
+      };
+    }
+
+    return {
+      success: true,
+      message: data.message || "success",
+      data: data.data,
+    };
+  } catch (err) {
+    return {
+      success: false,
+      message: err.message || "Network err",
+      errors: err,
+    };
+  }
+};
+
+export { getClientContracts };
