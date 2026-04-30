bd63ad5821273c89c498c315408654902ab0de9a
yassir laa <yassirlaa411@gmail.com>
2026-04-30 12:59:23 +0100

    create get conversation API endpoint

---
+++ backend/app/Http/Controllers/ConversationController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Conversation;
+use Illuminate\Http\Request;
+
+class ConversationController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        $user = $request->user();
+
+        $role = $user->role;
+
+        $query = Conversation::query()
+            ->select('id', 'contract_id', 'created_at');
+
+        $with = [
+            'contract:id,proposal_id,status',
+            'contract.proposal:id,project_id,freelancer_id',
+            'contract.proposal.project:id,client_id,title',
+        ];
+
+        if ($role === 'client') {
+            $with[] = 'contract.proposal.freelancer:id,user_id';
+            $with[] = 'contract.proposal.freelancer.user:id,first_name,last_name,avatar';
+
+            $query->whereHas('contract.proposal.project', function ($q) use ($user) {
+                $q->where('client_id', $user->id);
+            });
+        } else if ($role === 'freelancer') {
+            $with[] = 'contract.proposal.project.client:id,first_name,last_name,avatar';
+            $query->whereHas('contract.proposal.freelancer', function ($q) use ($user) {
+                $q->where('user_id', $user->id);
+            });
+        }
+
+        $conversations = $query
+            ->with($with)
+            ->latest()
+            ->get();
+
+        $data = $conversations->map(function ($conversation) use ($role) {
+            $contract = $conversation->contract;
+            $proposal = $contract->proposal;
+            $project = $proposal->project;
+
+            $otherUser = null;
+
+            if ($role === 'client') {
+                $otherUser = $proposal->freelancer->user;
+            } else if ($role === 'freelancer') {
+                $otherUser = $project->client;
+            }
+
+            return [
+                'id' => $conversation->id,
+                'created_at' => $conversation->created_at,
+                'contract_id' => $conversation->contract_id,
+                'contract_status' => $contract->status,
+
+                'project' => [
+                    'id' => $project->id,
+                    'title' => $project->title,
+                ],
+
+                'other_user' => [
+                    'id' => $otherUser->id,
+                    'first_name' => $otherUser->first_name,
+                    'last_name' => $otherUser->last_name,
+                    'avatar' => $otherUser->avatar
+                ]
+            ];
+        });
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Conversations retrieved successfully',
+            'data' => $data
+        ]);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\ClientContractController;
 use App\Http\Controllers\ClientDashboardController;
 use App\Http\Controllers\ClientProjectController;
 use App\Http\Controllers\ClientProposalController;
+use App\Http\Controllers\ConversationController;
 use App\Http\Controllers\FreelancerDashboardController;
 use App\Http\Controllers\FreelancerProjectController;
 use App\Http\Controllers\FreelancerProposalController;
@@ -33,8 +34,6 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/client/contracts', [ClientContractController::class, 'index']);
         Route::get('/client/contracts/stats', [ClientContractController::class, 'stats']);
         Route::get('/client/contracts/{id}', [ClientContractController::class, 'show']);
-
-
     });
 
     Route::middleware('role:freelancer')->group(function () {
@@ -45,6 +44,10 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/freelancer/proposals', [FreelancerProposalController::class, 'index']);
     });
 
+    // ? categories & skills
     Route::get('/categories', [CategoryController::class, 'index']);
     Route::get('/skills', [SkillController::class, 'index']);
+
+    // ? conversations 
+    Route::get('/conversations', [ConversationController::class, 'index']);
 });
