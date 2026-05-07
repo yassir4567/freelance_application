1e5816d5ffedff96ec28f79d9341d43cc1286fb1
yassir laa <yassirlaa411@gmail.com>
2026-05-07 19:02:08 +0100

    create card for if contract setup page unavailable

--- frontend/src/features/contracts/pages/ClientActiveContract.jsx
+++ frontend/src/features/contracts/pages/ClientActiveContract.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useMemo, useState } from "react";
 import styles from "../styles/ClientActiveContract.module.css";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import { getSetupContractInfo } from "../../../api/contracts/getSetupContractInfo";
 import ClientActiveContractHeader from "../components/ClientActiveContractHeader";
 import { useAuth } from "../../../context/AuthContext";
+import { FiArrowLeft } from "react-icons/fi";
 
 function ClientActiveContract() {
   const [setupInfo, setSetupInfo] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [success, setSuccess] = useState(false);
   const { user } = useAuth();
   const { contractId } = useParams();
 
@@ -15,20 +17,23 @@ function ClientActiveContract() {
     const loadSetupInfo = async () => {
       setIsLoading(true);
       const result = await getSetupContractInfo(contractId);
+      console.log(result);
       if (result.success) {
         setSetupInfo(result.data ?? null);
         setIsLoading(false);
+        setSuccess(result.success);
         return;
       }
 
       setIsLoading(false);
+      setSuccess(result.success);
     };
     loadSetupInfo();
   }, []);
 
   const freelancerInfo = useMemo(() => {
     const freelancer = setupInfo?.proposal?.freelancer;
-    
+
     return {
       id: freelancer?.id,
       user_id: freelancer?.user_id,
@@ -46,8 +51,27 @@ function ClientActiveContract() {
   }, [setupInfo]);
 
   const role = user.role;
+
+  if (!success || !setupInfo) {
+    return (
+      <div className={styles.contractSetUpPage}>
+        <div className={`${styles.stateCard} ${styles.errorState}`}>
+          <p className={styles.stateKicker}>unavailable</p>
+          <h1 className={styles.stateMessage}>We could not load this page.</h1>
+          <NavLink
+            to={`/dashboard/client/contracts`}
+            className={styles.backLink}
+          >
+            <FiArrowLeft />
+            <span>Back to contracts</span>
+          </NavLink>
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className={styles.container}>
+    <div className={styles.contractSetUpPage}>
       <ClientActiveContractHeader
         contractInfo={contractInfo}
         freelancerInfo={freelancerInfo}
